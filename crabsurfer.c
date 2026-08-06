5fed66947f88175364fe67c15f673ffd92b7ff11
Isocoram <lee.klese@icloud.com>
2026-08-06 06:00:01 +0200

    Remove manual next-message select functionality.

--- crabsurfer.c
+++ crabsurfer.c
@@ -5,25 +5,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>      // for ssize_t
-#include <stddef.h>     // for size_t
+#include <stddef.h>         // for size_t
 #include <unistd.h>
 #include <string.h>
 
 #define BUFFERSIZE 4096
 
-//typedef enum { FALSE, TRUE } bool;
-
 enum status { FAILED = -1, LISTEN_QUEUE_SIZE = 1 };
+
+char get_single_char(void) {
+  char buffer[10];
+  fgets(buffer, sizeof(buffer), stdin);
+  return buffer[0];
+}
 
 void serve(int socket_fd) {
   const char msg[] = "You've been served >:D\n";
-  ssize_t send_status = send(socket_fd, msg, sizeof(msg), MSG_CONFIRM);
+  char buffer[BUFFERSIZE];
+  for (size_t i = 0; i < sizeof(buffer); i++) {
+    buffer[i] = (char)'\0';
+  }
+  read(0, buffer, sizeof(buffer));
+  ssize_t send_status = send(socket_fd, buffer, sizeof(buffer), MSG_CONFIRM);
   if (send_status == FAILED) {
     return;
   }
 }
 
 void client_session(const char *ip, uint16_t port) {
+  next_iteration:
   int sock = socket(AF_INET, SOCK_STREAM, 0);
   if (sock == FAILED) return;
 
@@ -41,6 +51,7 @@ void client_session(const char *ip, uint16_t port) {
   else {
     char stream_data[BUFFERSIZE];
     while (1) {
+
       ssize_t receive_status = recv(sock, stream_data, sizeof(stream_data), 0);
       if (receive_status == FAILED) {
         perror("Cant Receive");
@@ -51,6 +62,11 @@ void client_session(const char *ip, uint16_t port) {
     }
   }
   stop:
+  //char character = get_single_char();
+  //if (character != 's') goto next_iteration;
+  goto next_iteration;
+
+  close(sock);
   return;
 }
 
@@ -93,13 +109,16 @@ int main(int argc, char **argv) {
   server_ip = destination_ip = port = file_path = NULL;
 
   const char *help =
-    "Usage: ./crabsurfer [flag] [ip] [port]\n"
-    "flags:\n"
-    "-h, help\n"
-    "-s, server mode\n"
-    "-c, client mode\n"
-    "-t, terminal chat mode\n"
-    "-f, file chat mode\n";
+    "Usage:\n"
+    "Server  ./crabsurfer -s <port>\n"
+    "Client  ./crabsurfer -c <destination ip> -p <destination port>\n\n"
+    "Flags:\n"
+    "-h  help\n"
+    "-s  specify server mode\n"
+    "-c  specify client mode\n"
+    "-p  specify port\n"
+    "-t  terminal chat mode\n"
+    "-f  file chat mode\n";
 
   while ((flag = getopt(argc, argv, "htf:sc:p:")) != -1) {
     switch (flag) {
@@ -111,9 +130,6 @@ int main(int argc, char **argv) {
         return 1;
       case 's':
         server_mode = true;
-        //if (optarg == NULL) break;
-        //server_ip = strdup(optarg);
-        //printf("Server IP: %s\n", server_ip);
         break;
       case 'c':
         destination_ip = strdup(optarg);
@@ -156,6 +172,8 @@ int main(int argc, char **argv) {
       }
 
       if (destination_ip && port) {
+        //printf("Press [s] to stop!\n");
+        fflush(stdout);
         client_session(destination_ip, port_number);
         goto done;
       }
